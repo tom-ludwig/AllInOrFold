f8e4a41629987d92c1e1207ecc6fc1d619f39697
Ludwig <tommludwig@icloud.com>
2025-04-28 10:04:07 +0200

    fix tests

--- src/test/kotlin/hwr/oop/group1/poker/DeckTest.kt
+++ src/test/kotlin/hwr/oop/group1/poker/DeckTest.kt
@@ -4,35 +4,40 @@ import io.kotest.core.spec.style.AnnotationSpec
 import org.assertj.core.api.Assertions.assertThat
 
 class DeckTest: AnnotationSpec() {
+    // Reflection to access private field
+    private fun getCards(deck: Deck): List<Card> {
+        // Bypass private access to check community cards
+        val field = Deck::class.java.getDeclaredField("cards")
+        field.isAccessible = true
+        val cards =  field.get(deck) as List<*>
+        return cards.filterIsInstance<Card>().takeIf { it.size == cards.size } ?: emptyList()
+    }
+
     @Test
     fun `Deck includes all cards`() {
         val deck = Deck()
 
-        deck.resetDeck()
-
-        assertThat(deck.cards.toSet()).hasSize(deck.cards.size).hasSize(52)
+        val cards = getCards(deck)
+        assertThat(cards.toSet()).hasSize(cards.size).hasSize(52)
     }
 
     @Test
     fun `Drawing draws top card and removes it`() {
         val deck = Deck()
 
-        deck.resetDeck()
-        val cardAtTop = deck.cards.first()
+        val cards = getCards(deck)
+        val cardAtTop = cards.first()
         val card = deck.draw()
 
         assertThat(cardAtTop).isEqualTo(card)
-        assertThat(deck.cards).doesNotContain(card)
+        assertThat(cards).doesNotContain(card)
     }
 
     @Test
-    fun `Shuffling randomises the deck`() {
+    fun `Constructed Decks are Shuffled`() {
         val deck = Deck()
-        deck.resetDeck()
+        val newDeck = Deck()
 
-        val deckOld = deck.cards.toList()
-        deck.shuffleDeck()
-
-        assertThat(deckOld).isNotEqualTo(deck.cards)
+        assertThat(getCards(deck)).isNotEqualTo(getCards(newDeck))
     }
 }
