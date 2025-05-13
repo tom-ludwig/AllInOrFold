3b8e9713c884dcff31776f44c13695be25c76176
bleikerja <bleikerja@gmail.com>
2025-05-13 16:31:55 +0200

    fix merge mistakes

--- src/main/kotlin/hwr/oop/group1/poker/Game.kt
+++ src/main/kotlin/hwr/oop/group1/poker/Game.kt
@@ -1,16 +1,11 @@
 package hwr.oop.group1.poker
 
 class Game {
-    private var round = Round()
+    var round = Round()
+        private set
     var players = emptyList<Player>().toMutableList()
         private set
-    private var dealerPosition = 0
-    
-    private fun nextDealer() {
-        dealerPosition = (dealerPosition + 1) % players.size
-    }
-    var dealerPosition = -1
-        private set
+    private var dealerPosition = -1
     companion object {
         const val SMALL_BLIND = 5
         const val BIG_BLIND = 10
@@ -42,6 +37,10 @@ class Game {
         players.forEach {it.resetFold()}
         round = Round()
         payBlinds()
+    }
+
+    private fun nextDealer() {
+        dealerPosition = (dealerPosition + 1) % players.size
     }
 
     fun dealer(): Player {
